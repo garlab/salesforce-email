e9efdf2ed6b944d742509f2b164ded20b7cc7420
Anthony GARCIA <anthony.garlab@gmail.com>
2018-11-14 17:47:23 +0000

    Use var

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const regex = /^[-+`{|}~a-z0-9!#$%&'*/=?_^][-+`{|}~a-z0-9!#$%&'*/=?_.^]*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/
+var regex = /^[-+`{|}~a-z0-9!#$%&'*/=?_^][-+`{|}~a-z0-9!#$%&'*/=?_.^]*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/
 
 exports.isValidEmail = function(email) {
   return !!String(email).match(regex)
